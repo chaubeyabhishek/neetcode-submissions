56e47746e13f6cea1efd21dfa90b93362188ec86
Abhishek Chaubey <151832058+chaubeyabhishek@users.noreply.github.com>
2026-09-04 23:12:20 +0530

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>tail;
+        for(int num:nums){
+            int left = 0;
+            int right = tail.size();
+            while(left < right){
+                int mid = left + (right - left)/2;
+                if(tail[mid] < num){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid;
+                }
+            }
+            
+            if(left == tail.size()){
+                tail.push_back(num);
+            }
+            else{
+                tail[left] = num;
+            }
+        }
+        return tail.size();
+    }
+};
